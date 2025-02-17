91a3a2058d364155f788c186fcfc5ab78cd48bac
Davidson <dehpacifico@gmail.com>
2025-02-17 20:17:00 -0300

    iniciando configuracao inicial do typeorm no nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ProdutoModule } from './produtos/produto.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
 
 @Module({
-  imports: [UsuarioModule, ProdutoModule],
+  imports: [UsuarioModule,
+    ProdutoModule,
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService] //config typeorm
+    })
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
